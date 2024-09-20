f3d577c3527b5ffd7b9e522efc7e525093a7cddf
FelipeFidelisA <146492055+FelipeFidelisA@users.noreply.github.com>
2024-09-20 11:28:54 -0300

    Background e header
    Background e header

---
+++ src/Pages/content.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './../styles/content_style.css'
+
+const Content: React.FC = () => {
+    return (
+        <div className='content'>
+            <div className='container'>
+                <header>
+                    <div>
+                        <img src="/riot-games.svg" width={52} height={26} alt="Riot Games Logo" />
+
+                        <span />
+
+                        <img src="/valorant.svg" width={32} height={26} alt="Valorant Logo" />
+                        <nav className='nav'>
+                            <a href="#">GAME</a>
+                            <a href="#">CHAMPIONS</a>
+                            <a href="#">NEWS</a>
+                            <a href="#">PATCH NOTES</a>
+                            <a href="#">SHOP</a>
+                            <a href="#">SUPPORT</a>
+                        </nav>
+                        <div className='header_user'>
+                            <img src="/user-icon.png" width={42} height={42} alt="User Icon" />
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </div>
+    )
+}
+
+export default Content
